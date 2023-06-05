bbc8c1db5efe886fbbd6b61ba98786cf14bda405
Asen <asen.trichkov1@gmail.com>
2023-06-06 01:22:07 +0300

    feat: add route

---
+++ src/routes/healthchecker/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const routes = express.Router();
+
+routes.use((req, res) => {
+    const healthcheck = {
+        uptime: process.uptime(),
+        responseTime: process.hrtime.bigint().toString(),
+        message: 'OK',
+        timestamp: Date.now()
+    };
+
+    try {
+        res.send(healthcheck);
+    } catch (error) {
+        // TODO notify via telegram
+        healthcheck.message = error;
+        res.status(503).send();
+    }
+});
+
+module.exports = routes;
